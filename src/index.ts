f5aec2b19be9b844d35446f32adc8a0318ac6b5c
Kristian Coulson <kristiancoulson@gmail.com>
2024-01-06 08:51:00 +0000

    post todo, get single todo and error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import hbs from "hbs";
-import { getTodos } from "./services/todo";
+import { createTodo, getTodo, getTodos } from "./services/todo";
+import { NotFoundError } from "./errors";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -10,10 +11,46 @@ hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static("src/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   const todos = getTodos();
   res.render("index", { todos });
+});
+
+app.post("/todos", (req: Request, res: Response) => {
+  createTodo({
+    title: req.body.title,
+  });
+
+  res.redirect("/");
+});
+
+app.get("/todos/:id", (req: Request, res: Response) => {
+  const todo = getTodo({ id: req.params.id });
+
+  if (!todo) {
+    throw new NotFoundError("Todo not found");
+  }
+
+  res.render("todo", { todo });
+});
+
+app.use(function (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (err instanceof NotFoundError) {
+    res.status(404).render("error", { status: 404, message: err.message });
+    return;
+  }
+
+  res
+    .status(500)
+    .render("error", { status: 500, message: "Something went wrong" });
 });
 
 app.listen(port, () => {
